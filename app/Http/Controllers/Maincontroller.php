409510044b5531c5b93bb20c8ae73867319e5976
ansilpa <ansil.pa99@gmail.com>
2021-05-30 17:15:15 +0530

    13

--- app/Http/Controllers/Maincontroller.php
+++ app/Http/Controllers/Maincontroller.php
@@ -627,6 +627,7 @@ class Maincontroller extends Controller
         $data=['LoggedUserInfo'=>admin::where('id','=',session('LoggedUser'))->first()];
         $order=collect(DB::TABLE('orderprdetails')->join('orderdetails','orderprdetails.id','=','orderdetails.oid')
         ->select('orderprdetails.*','orderdetails.*')
+        ->orderBy('orderprdetails.created_at','desc')
         ->get());
         
         return view('dash.orders',$data,compact('order'));
